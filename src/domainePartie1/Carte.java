ee271e3f30201da06ae065ee58b54177b8398438
syllia <syllia92@gmail.com>
2013-11-14 17:07:08 -0500

    Modification de Carte et noeud

--- src/domainePartie1/Carte.java
+++ src/domainePartie1/Carte.java
@@ -1,11 +1,9 @@
-package domainePartie1;
 
-import java.util.ArrayList;
-import domainePartie1.Position;
+
 import domainePartie1.Noeud;
-
-
+import domainePartie1.Position;
 import java.util.ArrayList;
+
 import java.util.Timer;
 
 public class Carte {
@@ -28,98 +26,150 @@ public class Carte {
 		{
 			
 		}
-		public Arc(Noeud src, Noeud dest, float dist) {
+		public Arc(Noeud src, Noeud dest, float dist)
+		{
 			this.m_source = src;
 			this.m_destination = dest;
 			this.m_longueur = dist;
 		}
-
-		public float reqLongueur() {
+		public float reqLongueur()
+		{
 			return this.m_longueur;
 		}
-
-		public Noeud reqNoeudSource() {
+		
+		public Noeud reqNoeudSource()
+		{
 			return this.m_source;
 		}
-
-		public Noeud reqNoeudDest() {
+		
+		public Noeud reqNoeudDest()
+		{
 			return this.m_destination;
 		}
-
-		public void asgLongueur(float dist) {
-			this.m_longueur = this.calculerLongueur();
+		
+		public void asgLongueur(float dist)
+		{
+			this.m_longueur = this.calculerLongueur(); 
 		}
-
-
-		private float calculerLongueur() {
-
-			float distX = (this.m_source.reqPosition().reqPositionX() - this.m_destination.reqPosition().reqPositionX());
+		
+		private float calculerLongueur()
+		{
+			float distX = (this.reqNoeudSource().reqPosition().reqPositionX() - this.reqNoeudDest().reqPosition().reqPositionX());
 			float distX1 = distX * distX;
-			float distY = (this.m_source.reqPosition().reqPositionY() - this.m_destination.reqPosition().reqPositionY());
-
-			float distY1 = distY * distY;
-			double somme = (double) (distY1 + distX1);
-			return (float) (Math.sqrt(somme));
+			float distY = (this.reqNoeudSource().reqPosition().reqPositionY() - this.reqNoeudDest().reqPosition().reqPositionY());
+			float distY1 = distY*distY;
+			double somme = (double)(distY1 + distX1);
+			return (float)(Math.sqrt(somme));
 		}
-
+		
 	}// fin classe arc
 
 
 
-	public void ajouterArc(Noeud noeudSource, Noeud noeudDest) {
-		if (this.m_listeDeNoeuds.contains(noeudSource)
-				&& this.m_listeDeNoeuds.contains(noeudDest)) {
-			Arc nouvelArc = new Arc(noeudSource, noeudDest, 0);
-			float dist = nouvelArc.calculerLongueur();
-			nouvelArc.asgLongueur(dist);
-			this.m_listeArcs.add(nouvelArc);
-
-		}
-
-		/*
-		 * else { throw }
-		 */
-
-	}
+	
 	
     public Carte(){
     	m_listeDeNoeuds = new ArrayList<Noeud>();
     	m_listeArcs = new ArrayList<Arc>();
     }
 
-	public void ajouterNoeud(Position position)
-	{
-		this.m_listeDeNoeuds.add(new Noeud(position));
-	}
 	public ArrayList<Noeud> reqListeNoeuds()
 	{
 		return this.m_listeDeNoeuds;
 	}
 	
+	public ArrayList<Arc> reqm_listeArcs()
+	{
+		return this.m_listeArcs;
+	}
+	
+	
+	public void ajouterNoeud(Noeud.Position pos)
+	{
+		Noeud noeud = new Noeud(pos);
+		this.m_listeDeNoeuds.add(noeud);
+	}
+	
+	public void ajouterArc(Noeud noeudSource, Noeud noeudDest)
+	{
+		if(this.m_listeDeNoeuds.contains(noeudSource) && this.m_listeDeNoeuds.contains(noeudDest))
+		{
+			Arc nouvelArc = new Arc(noeudSource, noeudDest, 0);
+			float dist = nouvelArc.calculerLongueur();
+			nouvelArc.asgLongueur(dist);
+			this.m_listeArcs.add(nouvelArc);
+			int index = this.m_listeDeNoeuds.indexOf(noeudSource);
+			this.m_listeDeNoeuds.get(index).reqListeArcsAdjacents().add(nouvelArc);
+		}
+		
 
 	
-
+	}
+	
 	public void enleverNoeud(Noeud noeud)
 	{
 		if(this.m_listeDeNoeuds.contains(noeud))
 		{
-			
+			this.m_listeDeNoeuds.get(this.m_listeDeNoeuds.indexOf(noeud)).reqListeArcsAdjacents().clear();//enlever la liste des noeuds adjacents
+			this.m_listeDeNoeuds.remove(noeud);//enlever le noeud
 		}
-
 	}
-
-	public Noeud plusProche(Noeud noeudSrc, ArrayList<Noeud> listNoeuds) {
-		return noeudSrc;
+	 
+	public Noeud plusProche(Noeud noeudSrc, ArrayList<Noeud> listNoeuds)
+	{
+		float res = 999999999;
+		Noeud noeudDest = new Noeud();
+		if(this.m_listeDeNoeuds.contains(noeudSrc))
+		{
+			for (int i = 0; i<listNoeuds.size(); i++)
+			{
+				trouverItineraire(noeudSrc, listNoeuds.get(i));
+				if(listNoeuds.get(i).reqCout()<res) 
+				{
+					res =listNoeuds.get(i).reqCout();
+					noeudDest = listNoeuds.get(i);
+				}
+			}			
+		
+		}
+		return noeudDest;
 	}
-
-
-
-
+	
+	private void _initialisationDijkstra(Noeud a)//noeud a doit appartenir à la carte
+	{
+		for (int i = 0; i<this.m_listeDeNoeuds.size(); i++)
+		{
+			if(this.m_listeDeNoeuds.get(i)==a)
+			{
+				this.m_listeDeNoeuds.get(i).setCout(0);
+			}
+			else
+			{
+				this.m_listeDeNoeuds.get(i).setCout(999999999);
+			}
+		}
+		
+	}
 	
 	public ArrayList<Arc> trouverItineraire(Noeud noeud1, Noeud noeud2)
 	{
+		this._initialisationDijkstra(noeud1);
+		
+		for(int i = 0; i<noeud1.reqListeArcsAdjacents().size(); i++)
+			
+		{
+			float dist =noeud1.reqListeArcsAdjacents().get(i).reqLongueur();
+			if(dist < noeud1.reqListeArcsAdjacents().get(i).reqNoeudDest().reqCout())
+			{
+				noeud1.reqListeArcsAdjacents().get(i).reqNoeudDest().setCout(dist);
+				noeud1.reqListeArcsAdjacents().get(i).reqNoeudDest().setPredecesseur(noeud1.reqListeArcsAdjacents().get(i));
+			}
+			
+		}
+		
+		
+		
 		return m_listeArcs;
-
 	}
 
 }
--- src/domainePartie1/Noeud.java
+++ src/domainePartie1/Noeud.java
@@ -1,37 +1,94 @@
 package domainePartie1;
 
+import domainePartie1.Carte;
 import java.util.ArrayList;
 
-import domainePartie1.Carte.Arc;
 
 
-public class Noeud {
-	private Position m_position;
-	private ArrayList<Arc> m_listeArcsAdjacents;
-	private float m_cout; // pour Dijkstra 
-	private Arc m_predecesseur;
-	
-	public void Noeud()
+
+
+
+public class Noeud 
+	//début classe noeud
 	{
-		this.m_cout = 0;
-		this.m_listeArcsAdjacents = new ArrayList<Arc>();
-		this.m_position = new Position(0,0);
-	}
-	
-	public Noeud(Position position){
-		this.m_position = position;
 		
-	}
+		private Position m_position;
+		private ArrayList<Carte.Arc> m_listeArcsAdjacents;
+		private float m_cout; // pour Dijkstra 
+		private Carte.Arc m_predecesseur;
+		
+		Noeud()
+		{
+			this.m_cout = 0;
+			this.m_listeArcsAdjacents = new ArrayList<Carte.Arc>();
+			this.m_position = new Position(0,0);
+			
+		}
+		
+		Noeud(Position pos)
+		{
+			this.m_position = pos;
+			this.m_cout = 0;
+			this.m_listeArcsAdjacents = new ArrayList<Carte.Arc>();
+		}
+		//Getters and setters noeuds
+		public Carte.Arc reqPredecesseur()
+		{
+			return this.m_predecesseur;
+		}
+		public void setPredecesseur(Carte.Arc arc)
+		{
+			this.m_predecesseur = arc;
+		}
+		
+		public ArrayList<Carte.Arc> reqListeArcsAdjacents()
+		{
+			return this.m_listeArcsAdjacents;
+		}
+		public float reqCout()
+		{
+			return this.m_cout;
+		}
+		
+		public void setCout(float r)
+		{
+			this.m_cout = r;
+		}
+		
+		public Position reqPosition()
+		{
+			return this.m_position;
+		}
+		
+		public void setPosition(Position nouvellePos)
+		{
+			m_position = nouvellePos;
+		}
+		//fin getters and setters noeuds
+		
 	
-	public Position reqPosition()
-	{
-		return this.m_position;
-	}
-	
-	public void setPosition(Position nouvellePos)
-	{
-		m_position = nouvellePos;
-	}
+		class Position 
+		//Début classe position
+		{
+			private float m_positionX;
+			private float m_positionY;
+			Position(float x, float y)
+			{
+				this.m_positionX = x;
+				this.m_positionY = y;
+			}
+			public float reqPositionX()
+			{
+				return this.m_positionX;
+			}
+			public float reqPositionY()
+			{
+				return this.m_positionY;
+			}
+			
+			
+		}//fin de la classe position
+		
+	}// fin classe noeud
 
 
-}
