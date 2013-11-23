20c0db481db1359f47445e2703f174f160aad7f3
Trispa <trispa88@gmail.com>
2013-11-23 04:36:04 -0500

    ajoute de path defaut pour les icone des bouton plays .pause et teriner

--- src/domainePartie1/Default.java
+++ src/domainePartie1/Default.java
@@ -7,12 +7,19 @@ public class Default {
 	public static final String VEHICULE_IMAGE_PATH = "/images/VehiculeUrgence.png";
 	public static final String SETTINGS_ICON_PATH = "/images/settings.png";
 	public static final String SOURIS_ICON_PATH = "/images/souris.png";
+	public static final String PLAYS_IMAGE_PATH = "/images/play.png";
+	public static final String PAUSE_IMAGE_PATH = "/images/pause.png";
+	public static final String TERMINER_IMAGE_PATH = "/images/terminer.png";
+	public static final String URGENCE_IMAGE_PATH = "/images/urgence.png";
+	
 	public static final String ZOOMMOINS = "-";
 	public static final String ZOOMPLUS = "+";
 	public static final String STRATEGIE_PROX = "Stratégie de proximité";
 	public static final String STRATEGIE_ANC = "Stratégie d'ancienneté";
 	public static final String STRATEGIE_MIN = "Stratégie de chemin minimal";
 	public static final String STRATEGIE_GESTION = STRATEGIE_ANC;
+	
+	
 	
 	public static final int WIDTH_NOEUD = 20;
     public static final int HEIGHT_NOEUD = 20;
