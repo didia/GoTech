087e40d40bec1966d48c38d25839fd04466fea22
jsletellier <js.letellier@gmail.com>
2013-11-23 15:07:20 -0500

    Code review Echelle

--- src/domainePartie1/Echelle.java
+++ src/domainePartie1/Echelle.java
@@ -1,44 +1,67 @@
+/**
+* Cette classe fait la conversion des pixel en metre selon la valeur indiquee.
+*
+*
+* @version 1.0
+*
+* @author GoTech
+*
+*
+*/
+
+
+
+
 package domainePartie1;
 
-public class Echelle {
+public class Echelle 
+{
 	private int prevMetreParStep;
 	private int m_metreParStep = Default.METRE_PAR_STEP;
 	
-	public Echelle(){
+	public Echelle()
+	{
 		
 	}
 	
-	public int reqMetreParStep(){
+	public int reqMetreParStep()
+	{
 		return m_metreParStep;
 	}
-	public void setMetreParStep(int metreParStep){
+	
+	public void setMetreParStep(int metreParStep)
+	{
 		prevMetreParStep = m_metreParStep;
 		m_metreParStep = metreParStep;
 	}
 	
-	public Position reqPositionEnMetre(Position positionEnStep){
-		
+	public Position reqPositionEnMetre(Position positionEnStep)
+	{	
 		float posX = positionEnStep.reqPositionX() *m_metreParStep;
 		float posY = positionEnStep.reqPositionY()* m_metreParStep;
 		
 		return new Position(posX, posY);
 	}
 	
-	public Position reqPositionEnStep(Position positionEnMetre){
+	public Position reqPositionEnStep(Position positionEnMetre)
+	{
 		
 		float posX = positionEnMetre.reqPositionX()/m_metreParStep;
 		float posY = positionEnMetre.reqPositionY()/m_metreParStep;
 		
 		return new Position(posX, posY);
 	}
-	private Position reqPositionEnOldStep(Position positionEnMetre){
+	
+	private Position reqPositionEnOldStep(Position positionEnMetre)
+	{
 		float posX = positionEnMetre.reqPositionX()/prevMetreParStep;
 		float posY = positionEnMetre.reqPositionY()/prevMetreParStep;
 		
 		return new Position(posX, posY);
 	}
 	
-	public Position reqUpdatedPosition(Position position){
+	public Position reqUpdatedPosition(Position position)
+	{
 		return reqPositionEnMetre(reqPositionEnOldStep(position));
 	}
 	
