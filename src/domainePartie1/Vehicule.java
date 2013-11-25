25b1ad5ee65a0b08f17c1d4f96936c65a405851d
Trispa <trispa88@gmail.com>
2013-11-25 05:15:22 -0500

    avoir versin vehicule qui bouge normalement

--- src/domainePartie1/Vehicule.java
+++ src/domainePartie1/Vehicule.java
@@ -3,7 +3,7 @@ package domainePartie1;
 import java.util.ArrayList;
 
 public class Vehicule {
-	
+
 	private static Vehicule m_vehicule = new Vehicule();
 	private Noeud m_portAttache = null;
 	private float m_vitesse = 0;
@@ -14,131 +14,124 @@ public class Vehicule {
 	private Noeud m_noeudDestination = null;
 	private Carte m_gps;
 	private ArrayList<Noeud> m_itineraireActuel = null;
-	
-	
-	
-	
-	//constructeur priv�e vehicule
-	
-	private Vehicule(){
-		
-		
+
+	// constructeur priv�e vehicule
+
+	private Vehicule() {
+
 	}
-	
-	
-	//Obtenir le v�hicule
-	public static Vehicule getInstance()
-	{
+
+	// Obtenir le v�hicule
+	public static Vehicule getInstance() {
 		return m_vehicule;
 	}
-	
-	public void lancerMission(GestionnaireUrgence gestionnaire, float vitesse, Carte gps){
+
+	public void lancerMission(GestionnaireUrgence gestionnaire, float vitesse,
+			Carte gps) {
 		m_gestionnaireUrgence = gestionnaire;
 		m_vitesse = vitesse;
 		m_gps = gps;
 	}
-	
-	public void asgPointAttache(Noeud noeud)
-	{
+
+	public void asgPointAttache(Noeud noeud) {
 		m_portAttache = noeud;
 		m_noeudActuel = m_portAttache;
 		m_position = m_portAttache.reqPosition();
 	}
-	public void asgGestionnaireUrgence(GestionnaireUrgence gps){
+
+	public void asgGestionnaireUrgence(GestionnaireUrgence gps) {
 		m_gestionnaireUrgence = gps;
 	}
-	
-	public void AllerVers(Noeud p_noeudDestination)
-	{
-		//le Noeud actuel devien le noeud de destination 
+
+	public void AllerVers(Noeud p_noeudDestination) {
+		// le Noeud actuel devien le noeud de destination
 		this.asgNoeudActuel(p_noeudDestination);
 	}
-	
-	public void allerPortAttache()
-	{
-		//le Noeud actuel devien le noeudAttache
+
+	public void allerPortAttache() {
+		// le Noeud actuel devien le noeudAttache
 		this.asgNoeudActuel(this.m_portAttache);
 	}
-	
-	public Position reqPosition()
-	{
+
+	public Position reqPosition() {
 		return this.m_position;
 	}
-	
-	public Noeud reqPortAttache()
-	{
+
+	public Noeud reqPortAttache() {
 		return this.m_portAttache;
 	}
 
-	public  float reqVistess()
-	{
+	public float reqVistess() {
 		return this.m_vitesse;
 	}
-	
-	public Noeud reqnoeudActuel()
-	{
+
+	public Noeud reqnoeudActuel() {
 		return m_noeudActuel;
 	}
-	//TODO
-	public Arc arcActuel()
-	{
-		return  m_arcActuel = new Arc(m_noeudActuel, m_noeudDestination);
+
+	// TODO
+	public Arc arcActuel() {
+		return m_arcActuel = new Arc(m_noeudActuel, m_noeudDestination);
 	}
-	
-	public void asgNoeudActuel(Noeud noeud)
-	{
-		this.m_noeudActuel= noeud;
+
+	public void asgNoeudActuel(Noeud noeud) {
+		this.m_noeudActuel = noeud;
 		this.m_position = noeud.reqPosition();
 	}
-	public void asgPositin(Position unePosition)
-	{
+
+	public void asgPositin(Position unePosition) {
 		this.m_position = unePosition;
 	}
-	public void avance(int duree){
+
+	public void avance(int duree) {
 		// ne tiens pas encore compte du chemin
-		
-	nouvellePositionVoiture(duree );//TODO
-		
 
-		
-		if(this.m_itineraireActuel != null && !this.m_itineraireActuel.isEmpty()){
+		nouvellePositionVoiture(duree);// TODO
+
+		if (this.m_itineraireActuel != null
+				&& !this.m_itineraireActuel.isEmpty()) {
 			this.poursuisChemin();
-		}
-		else{
-			m_noeudDestination = this.m_gestionnaireUrgence.reqProchainNoeudATraite();
+		} else {
+			m_noeudDestination = this.m_gestionnaireUrgence
+					.reqProchainNoeudATraite();
 
-			if(m_noeudDestination != null){
-				this.m_itineraireActuel = this.m_gps.trouverItineraire(m_noeudActuel, m_noeudDestination);
+			if (m_noeudDestination != null) {
+				this.m_itineraireActuel = this.m_gps.trouverItineraire(
+						m_noeudActuel, m_noeudDestination);
 				this.poursuisChemin();
 			}
 		}
-		
+
 	}
-	private void poursuisChemin(){
+
+	private void poursuisChemin() {
 		Noeud noeud = this.m_itineraireActuel.get(0);
 		this.asgNoeudActuel(noeud);
 		this.m_itineraireActuel.remove(noeud);
-		if((m_noeudActuel.reqPosition().reqPositionX() == m_noeudDestination.reqPosition().reqPositionX()) &&
-				(m_noeudActuel.reqPosition().reqPositionY() == m_noeudDestination.reqPosition().reqPositionY())){
+		if ((m_noeudActuel.reqPosition().reqPositionX() == m_noeudDestination
+				.reqPosition().reqPositionX())
+				&& (m_noeudActuel.reqPosition().reqPositionY() == m_noeudDestination
+						.reqPosition().reqPositionY())) {
 			this.m_gestionnaireUrgence.traiterUrgenceActuelle();
 			this.m_itineraireActuel = null;
 			this.m_noeudDestination = null;
 		}
 	}
-	
-	public Position nouvellePositionVoiture(int duree )
-	{
-		//TODO
+
+	public Position nouvellePositionVoiture(int duree) {
+		// TODO
 		Position position = null;
 		float newHypotenuse = this.m_vitesse * duree;
-		
-		float newPositionX  =( m_arcActuel.reqNoeudDest().reqPosition().reqPositionX() *newHypotenuse) /m_arcActuel.reqLongueur();
-		float newPositionY  = ( m_arcActuel.reqNoeudDest().reqPosition().reqPositionY() * newHypotenuse) /m_arcActuel.reqLongueur();
-position = new Position(newPositionX,newPositionY);
+
+		float newPositionX = (m_arcActuel.reqNoeudDest().reqPosition()
+				.reqPositionX() * newHypotenuse)
+				/ m_arcActuel.reqLongueur();
+		float newPositionY = (m_arcActuel.reqNoeudDest().reqPosition()
+				.reqPositionY() * newHypotenuse)
+				/ m_arcActuel.reqLongueur();
+		position = new Position(newPositionX, newPositionY);
 		this.asgPositin(position);
 		return position;
-		
-		
-		
+
 	}
 }
