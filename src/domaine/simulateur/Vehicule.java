acbbf2dbd52b83f96c5569c0a86ea26fd3902de4
Aristote Diasonama <aristotediasonama@yahoo.fr>
2013-12-18 15:16:33 -0500

    Resoudre le problème du noeud actuel

--- src/domaine/simulateur/Vehicule.java
+++ src/domaine/simulateur/Vehicule.java
@@ -212,6 +212,7 @@ public class Vehicule  implements Serializable{
 		Noeud noeud = this.m_itineraireActuel.get(0);
 	
 		distanceDuProchainNoeud = this.m_noeudActuel.cout(noeud);
+		
 		float y2 = m_itineraireActuel.get(0).reqPosition().reqPositionY();
 		float x2 = m_itineraireActuel.get(0).reqPosition().reqPositionX();
 		float x1 = this.m_position.reqPositionX();
